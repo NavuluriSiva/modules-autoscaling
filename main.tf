73c9310a4aff3917ebd0fd497ae1791f80ed0de8
NavuluriSiva <sivalakshmi.v63@gmail.com>
2026-02-20 15:43:01 +0530

    modules in

--- main.tf
+++ main.tf
@@ -1,12 +1,28 @@
-provider "aws" {
-  region     = "ap-south-1"
+ resource "aws_launch_template" "this" {
+  name_prefix   = "asg-template-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+
+  lifecycle {
+    create_before_destroy = true
   }
-module "autoscaling" {
-  source           = "./modules/autoscaling"
-  ami_id           = var.ami_id
-  instance_type    = var.instance_type
+ }
+ 
+ resource "aws_autoscaling_group" "this" {
   desired_capacity = var.desired_capacity
-  min_size         = var.min_size
   max_size         = var.max_size
-  subnet_ids       = var.subnet_ids
+  min_size         = var.min_size
+
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.this.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg"
+    propagate_at_launch = true
+  }
 }
